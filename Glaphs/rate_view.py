b81315035d0aaeb234087154a1b9e3d071ae327f
gorillabbit <modsyoukaizenryoku@gmail.com>
2021-09-03 07:22:08 +0900

    評価数と再生数の関係

--- Glaphs/rate_view.py
+++ Glaphs/rate_view.py
@@ -2,6 +2,7 @@ from glob import glob
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
 def change_length(x):
     element = str(x).split(':')
     result = int(element[0]) * 60 + int(element[1]) + int(element[2]) / 60
@@ -9,23 +10,37 @@ def change_length(x):
     return result
 
 
-src = glob('H:/Projects/basedata_KUN/*.xlsx')[-1]  # エクセルファイルからデータ取得
-skiprows = list(range(1,6000))
-df_base = pd.read_excel(src, sheet_name='base', skiprows=skiprows)
-df_base['高評価数(再生数比x1000)'] = df_base['高評価数']/df_base['再生数'] * 1000
-df_base['低評価数(再生数比x1000)'] = df_base['低評価数']/df_base['再生数'] * 1000
-df_base['コメント数(再生数比x1000)'] = df_base['コメント数']/df_base['再生数'] * 1000
-df_base['高評価-低評価比率'] = df_base['高評価数']/df_base['低評価数']
-df_base['長さ(分)'] = df_base['長さ'].apply(change_length)
-df_base['投稿日'] = df_base['投稿日時'].apply(lambda x: x[0:10].replace('/', '-'))
+def make_basedata():
+    src = glob('H:/Projects/basedata_KUN/*.xlsx')[-1]  # エクセルファイルからデータ取得
+    skiprows = list(range(1, 6000))
+    df_base = pd.read_excel(src, sheet_name='base', skiprows=skiprows)
+    df_base['高評価数(再生数比x1000)'] = df_base['高評価数'] / df_base['再生数'] * 1000
+    df_base['低評価数(再生数比x1000)'] = df_base['低評価数'] / df_base['再生数'] * 1000
+    df_base['コメント数(再生数比x1000)'] = df_base['コメント数'] / df_base['再生数'] * 1000
+    df_base['高評価-低評価比率'] = df_base['高評価数'] / df_base['低評価数']
+    df_base['長さ(分)'] = df_base['長さ'].apply(change_length)
+    df_base['投稿日'] = df_base['投稿日時'].apply(lambda x: x[0:10].replace('/', '-'))
+    df_base.to_excel('H:/Projects/Glaphs/basedata.xlsx')
 
+
+# make_basedata()
+df_base = pd.read_excel('H:/Projects/Glaphs/basedata.xlsx')
 print(df_base)
 font = 'MS Gothic'
 plt.style.use('ggplot')
 fig = plt.figure(figsize=(12.8, 7.2), tight_layout=True)
-plt.title('再生数と1000再生あたりの高評価数の関係', fontname=font)
-plt.scatter(df_base['再生数'], df_base['高評価数(再生数比x1000)'], s=2)
-fig.savefig('H:/Projects/Glaphs/like_rate_and_view.png')
-#plt.xscale("log")
-plt.show()
 
+# plt.title('再生数と1000再生あたりの高評価数の関係', fontname=font)
+# plt.scatter(df_base['再生数'], df_base['高評価数(再生数比x1000)'], s=2)
+# fig.savefig('H:/Projects/Glaphs/like_rate_and_view.png')
+
+#plt.title('再生数と1000再生あたりの低評価数の関係', fontname=font)
+#plt.scatter(df_base['再生数'], df_base['低評価数(再生数比x1000)'], s=2)
+#plt.ylim(0, 5)
+#fig.savefig('H:/Projects/Glaphs/unlike_rate_and_view.png')
+
+plt.title('再生数と高-低評価比率の関係', fontname=font)
+plt.scatter(df_base['再生数'], df_base['高評価-低評価比率'], s=2)
+fig.savefig('H:/Projects/Glaphs/like_unlike_rate_and_view.png')
+plt.xscale("log")
+plt.show()

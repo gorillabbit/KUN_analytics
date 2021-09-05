7e95ad42d6f4a79e2702f626790b3d0df825d105
gorillabbit <modsyoukaizenryoku@gmail.com>
2021-09-06 02:48:31 +0900

    executeのimport

--- weekly_KUN_analytics/execute.py
+++ weekly_KUN_analytics/execute.py
@@ -1,5 +1,10 @@
-import datetime
+import make_weekly_xlsx
+import download_thumbnail
+import Graph
+import makepptx
+import copy_videos
 
+import datetime
 import logging
 
 nowtime = str(datetime.datetime.now()).replace(':', '-').replace('.', '-')
